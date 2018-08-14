65fd975cbe0399f01c81ea4603412a5b70211b63
shzhang <szhang4@gradcenter.cuny.edu>
2018-08-14 15:03:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
       'MarkupSafe', 'matplotlib', 'numpy', 'opencv-python', 'pandas',
       'pandas_summary', 'pickleshare', 'Pillow', 'plotnine',
       'ptyprocess', 'Pygments', 'pyparsing', 'python-dateutil', 'pytz', 'PyYAML', 'pyzmq', 'scipy',
-      'seaborn', 'simplegeneric', 'sklearn_pandas', 'testpath', 'torch<0.4', 'torchtext', 'torchvision', 'tornado', 'tqdm',
+      'seaborn', 'simplegeneric', 'sklearn_pandas', 'testpath', 'torch', 'torchtext', 'torchvision', 'tornado', 'tqdm',
       'traitlets', 'wcwidth', 'webencodings', 'widgetsnbextension'],
     keywords = ['deeplearning', 'pytorch', 'machinelearning'],
     classifiers = ['Development Status :: 3 - Alpha',
